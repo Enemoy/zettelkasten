4e8adc610901dbc51bc07be9491179fc0ef7f80b
Enemoy <sw@hsbach.de>
2025-08-12 09:30:56 +0200

    Fixed missing path for code when script is called externally.

--- dmenu_convert_to_org_clip.sh
+++ dmenu_convert_to_org_clip.sh
@@ -11,13 +11,17 @@ target_dir="$HOME/Downloads/"
 # text=$(cat $target_dir$(find $target_dir -type f  -printf "%P\n" | $DROPDOWN_MENU))
 text="$target_dir$(find $target_dir -type f  -printf "%P\n" | $DROPDOWN_MENU)"
 
-text=$(python3 convert_bib_to_org_handler.py $text)
+text=$(python3 ${Str_path_sourcecode}convert_bib_to_org_handler.py $text)
+
+notify-send "Target Dir" "$target_dir"
+notify-send "Content" "$text"
 
 if [[ -z "$text" ]];
 then
 	echo "Conversion didn't work, text is empty!"
 else
 	echo "$text" | wl-copy
+	# echo "$text"
 fi
 
 exit 0
